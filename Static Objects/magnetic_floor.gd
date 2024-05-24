05296a8d890f8e5f0980f7a623f551f1a19d7270
Ainsley <vhtmscyo@gmail.com>
2024-05-24 17:49:55 +0800

    添加Capturable

--- Static Objects/magnetic_floor.gd
+++ Static Objects/magnetic_floor.gd
@@ -3,6 +3,14 @@ extends StaticBody3D
 @export var plority: Magnetic.Plority
 @onready var magnetic: Magnetic = $Magnetic
 
+var captured_body: RigidBody3D
+
 func _ready() -> void:
 	if magnetic:
 		magnetic.current_plority = plority
+
+
+func _on_captured_area_body_entered(body: Node3D) -> void:
+	if body is RigidBody3D and !captured_body:
+		captured_body = body as RigidBody3D
+		
