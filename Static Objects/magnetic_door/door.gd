10bad1b5bd565715ff05174d0199406e4c0aaed5
Ainsley <vhtmscyo@gmail.com>
2024-06-14 17:14:47 +0800

    add change color

--- Static Objects/magnetic_door/door.gd
+++ Static Objects/magnetic_door/door.gd
@@ -3,13 +3,15 @@ extends AnimatableBody3D
 
 var has_initialized := false
 var is_magnetic: bool
-var current_plority: Magnetic.Plority
 var magnetic: Magnetic
 
 const MAGNETIC = preload("res://Utilities/magnetic.tscn")
 const EXTRACTABLE = preload("res://Utilities/extractable.tscn")
 
 @onready var magnetic_field: MagneticField = $MagneticField
+@onready var mesh_manager: MeshManager = $MeshManager
+@onready var mesh: MeshInstance3D = $CollisionShape3D/Mesh
+
 
 func initialize(data: MagneticData):
 	if !data:
@@ -17,14 +19,20 @@ func initialize(data: MagneticData):
 	if has_initialized: 
 		printerr(name, " in ", owner.name, " has been initialized twice")
 	is_magnetic = data.is_magnetic
+	
 	if data.is_magnetic:
 		magnetic = MAGNETIC.instantiate()
 		add_child(magnetic)
 		magnetic_field.magnetic = magnetic
+		mesh_manager.connect_magnetic(magnetic)
+		
 	if data.is_extractable:
 		var extractable = EXTRACTABLE.instantiate()
 		extractable.magnetic = magnetic
 		add_child(extractable)
-	current_plority = data.initial_plority
-		
+		mesh.mesh.material.emission_enabled = true
+	magnetic.current_plority = data.initial_plority
 	has_initialized = true
+	
+
+	
