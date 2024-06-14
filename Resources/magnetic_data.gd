a5fdde9024e8b6bfdd863056af2a8a70410c72e0
Ainsley <vhtmscyo@gmail.com>
2024-06-14 15:30:27 +0800

    Add magnetic door

--- Resources/magnetic_data.gd
+++ Resources/magnetic_data.gd
@@ -1,5 +1,6 @@
 class_name MagneticData
 extends Resource
 
+@export var is_magnetic: bool
 @export var initial_plority: Magnetic.Plority
 @export var is_extractable: bool
---
+++ Static Objects/magnetic_door/magnetic_door.gd
@@ -0,0 +1,18 @@
+class_name MagneticDoorSuit
+extends Node3D
+
+@export_group("Left Door")
+@export var left_door_config: MagneticData
+@export var left_door_trigger: Node3D # TODO: Change to plority_change_trigger
+
+@export_group("Right Door")
+@export var right_door_config: MagneticData
+@export var right_door_trigger: Node3D # TODO: Change to plority_change_trigger
+
+
+@onready var left_door: MagneticDoor = $Model/LeftDoor
+@onready var right_door: MagneticDoor = $Model/Rightdoor
+
+func _ready() -> void:
+	left_door.initialize(left_door_config)
+	right_door.initialize(right_door_config)
