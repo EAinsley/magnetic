045d96af39d2cedbe3e27804fb2b4f716a8cff08
skdzqy <zengqy@shanghaitech.edu.cn>
2024-06-17 22:03:44 +0800

    new level 1

--- Player/corsshair.gd
+++ Player/corsshair.gd
@@ -8,7 +8,7 @@ var _is_colliding := false
 func _draw() -> void:
 	print("is colliding:", _is_colliding)
 	if _is_colliding:
-		draw_arc(Vector2.ZERO, circle_size, 0, PI * 2, 32, Color.RED, circle_width, true)
+		draw_arc(Vector2.ZERO, circle_size, 0, PI * 2, 32, Color.GREEN_YELLOW, circle_width, true)
 	else:	
 		print("draw red")
 		draw_arc(Vector2.ZERO, circle_size, 0, PI * 2, 32, Color.WHITE, circle_width, true)
