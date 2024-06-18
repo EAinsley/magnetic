85e0c993b7aa4baa8335a17913a71c2238cba6d0
ZZZZZ <1940315068@qq.com>
2024-06-18 22:32:25 +0800

    update all levels

--- Player/corsshair.gd
+++ Player/corsshair.gd
@@ -8,10 +8,10 @@ var _is_colliding := false
 func _draw() -> void:
 	print("is colliding:", _is_colliding)
 	if _is_colliding:
-		draw_arc(Vector2.ZERO, circle_size, 0, PI * 2, 32, Color.YELLOW, circle_width, true)
+		draw_arc(Vector2.ZERO, circle_size, 0, PI * 2, 32, Color.WHITE, circle_width, true)
 	else:	
 		print("draw red")
-		draw_arc(Vector2.ZERO, circle_size, 0, PI * 2, 32, Color.LIGHT_GRAY, circle_width, true)
+		draw_arc(Vector2.ZERO, 0.1, 0, PI * 2, 32, Color.WHITE, circle_width, true)
 
 
 
